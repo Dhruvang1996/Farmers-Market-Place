0c7146635742449a65169a44db04eb8a83ba8d70
Dhruvang1996 <dhruvang.merchant@ucalgary.ca>
2021-06-15 12:03:10 -0600

    Feat: Vercel Initial

--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,37 @@
 import Navbar from '../components/Navbar';
+import React,{ useEffect, useState } from 'react';
+import {profile} from '../Service/apiService';
 
 const cart = ( { isAuthenticated, setIsAuthenticated } ) => {
-    return (
-      <div>
-          <Navbar setIsAuthenticated={setIsAuthenticated} />
-          <div>
-            <h1>Cart</h1>
-          </div>
-      </div>
-    )
+
+  const initialState1 = {
+    firstName: '',
+    lastName: '',
+  };
+
+  const [user, setUser] = useState(initialState1);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    getUser(accessToken);
+    setUser(initialState1);
+  },[]);
+
+  const getUser = async (accessToken) => {
+    const userProfile = await profile(accessToken)
+    if (userProfile) {
+        setUser(userProfile);
+    }
+  }
+
+  return (
+    <div>
+        <Navbar setIsAuthenticated={setIsAuthenticated} user={user}/>
+        <div>
+          <h1>Cart</h1>
+        </div>
+    </div>
+  )
 }
   
 export default cart;
